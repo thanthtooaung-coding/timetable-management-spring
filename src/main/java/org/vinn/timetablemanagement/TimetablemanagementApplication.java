aa44c20f6b43eefe1586d7bbea15df9585cac9c3
thanthtooaung <thanthtoo1285@gmail.com>
2025-12-07 12:55:49 +0700

    :sparkles: feat: setup project

---
+++ src/main/java/org/vinn/timetablemanagement/TimetablemanagementApplication.java
@@ -0,0 +1,13 @@
+package org.vinn.timetablemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetablemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetablemanagementApplication.class, args);
+	}
+
+}
